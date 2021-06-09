0291dd12008c35bc33e1e55c7b1ac0984e783c0e
daslu <daniel.slutsky@gmail.com>
2021-06-09 18:54:08 +0300

    version update

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject notespace-sicmutils-examples "0.18.1"
+(defproject notespace-sicmutils-examples "0.19.2"
   :description "Demonstrating the use of Sicmutils with Notespace"
   :url "github.com/scicloj/notespace-sicmutils-example"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
